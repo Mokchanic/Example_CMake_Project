c96621aedd48d860ea92a1b1ff32f685d4a4fa94
Mokchanic <mokok118@kakao.com>
2023-12-29 16:10:38 +0900

    init_CMake_Base_Project

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 Mokchanic, All Rights Reserved.
+
+/**
+ * @file main.cpp
+ * @author JinWoo Mok
+ * @brief Example_CMake_Project
+ * @version 1.0
+ * @date 2023-12-27
+ */
+
+#include <iostream>
+#include <cstdint>
+
+int32_t main(){
+    std::cout << "Hello CMake Project!" << std::endl;
+    return 0;
+}
